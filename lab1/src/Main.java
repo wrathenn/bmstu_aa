fe154d607be1828b9732c88be8f6461de43b8a32
Rostislav <kluskape@gmail.com>
2021-09-20 00:42:47 +0300

    lab_1 tests and algs

---
+++ lab1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        StringDistanceAlgorithmsTester tester = new StringDistanceAlgorithmsTester("user", "loser", 10000000, 1000000);
+        tester.testAllAlgs();
+    }
+}
